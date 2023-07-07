93edc87d9fd69156e370ad19e878539cd31e0b5d
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2023-07-07 21:49:15 +0530

    menue class added to master

---
+++ menue.java
@@ -0,0 +1,15 @@
+public class menue {
+    public static void main(String[] args){
+        System.out.println("   +++++++++++++++++++++++++++");
+        System.out.println("   +                         +");
+        System.out.println("   +        Menue            +");
+        System.out.println("   +                         +");
+        System.out.println("   +    1. Addition          +");
+        System.out.println("   +    2. Subtraction       +");
+        System.out.println("   +    3. Multiplication    +");
+        System.out.println("   +    4. Division          +");
+        System.out.println("   +                         +");
+        System.out.println("   +++++++++++++++++++++++++++");
+        
+    }
+}
