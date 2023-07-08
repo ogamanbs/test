2604176a17f5e3085a0c40208528bd4f2429c8d2
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2023-07-08 19:18:35 +0530

    Calc extends Menue and prints Menue also

--- Calc.java
+++ Calc.java
@@ -1,5 +1,7 @@
-public class Calc{
+public class Calc extends Menue{
     public static void main(String[] args){
-        System.out.println("Calculator");
+        System.out.println("\n\t   Calculator\n");
+        Calc obj = new Calc();
+        obj.menueCard();
     }
 }
